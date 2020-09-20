821714256a8b5943a8e389a868c172c6228cd4b8
Jonathon Bowyer <jwbowyer@gmail.com>
2020-09-20 00:25:49 -0500

    Fixed bug around chunk/voxel in world checks

--- Assets/Scripts/CompanyNine/Voxel/Chunk/Chunk.cs
+++ Assets/Scripts/CompanyNine/Voxel/Chunk/Chunk.cs
@@ -6,84 +6,62 @@ namespace CompanyNine.Voxel.Chunk
 {
     public class Chunk
     {
-        private readonly GameObject chunkObject;
-        private readonly MeshRenderer meshRenderer;
-        private readonly MeshFilter meshFilter;
-        private readonly World world;
-        private int vertexIndex;
-        private readonly List<Vector3> vertices = new List<Vector3>();
-        private readonly List<int> triangles = new List<int>();
-        private readonly List<Vector2> uvs = new List<Vector2>();
+        private readonly GameObject _chunkObject;
+        private readonly MeshRenderer _meshRenderer;
+        private readonly MeshFilter _meshFilter;
+        private readonly World _world;
+        private int _vertexIndex;
+        private readonly List<Vector3> _vertices = new List<Vector3>();
+        private readonly List<int> _triangles = new List<int>();
+        private readonly List<Vector2> _uvs = new List<Vector2>();
 
-        private readonly ushort[,,] blockIdArray =
-            new ushort[VoxelData.ChunkWidth, VoxelData.ChunkHeight,
-                VoxelData.ChunkWidth];
-
-        private readonly ushort[][][] blockIdArray2 =
+        private readonly ushort[][][] _blockIdArray =
             new ushort[VoxelData.ChunkWidth][][];
 
         public Chunk(World world, ChunkCoordinate chunkCoordinate)
         {
-            this.world = world;
-            chunkObject = new GameObject();
-            chunkObject.transform.SetParent(world.transform);
-            chunkObject.transform.position = new Vector3(
+            this._world = world;
+            _chunkObject = new GameObject();
+            _chunkObject.transform.SetParent(world.transform);
+            _chunkObject.transform.position = new Vector3(
                 chunkCoordinate.X * VoxelData.ChunkWidth, 0,
                 chunkCoordinate.Z * VoxelData.ChunkWidth);
-            chunkObject.name = $"Chunk[{chunkCoordinate}]";
+            _chunkObject.name = $"Chunk[{chunkCoordinate}]";
 
             // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
-            meshFilter = chunkObject.AddComponent<MeshFilter>();
+            _meshFilter = _chunkObject.AddComponent<MeshFilter>();
             // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
-            meshRenderer = chunkObject.AddComponent<MeshRenderer>();
-            meshRenderer.material = world.Material;
+            _meshRenderer = _chunkObject.AddComponent<MeshRenderer>();
+            _meshRenderer.material = world.Material;
 
-            PopulateVoxelMap2();
+            PopulateVoxelMap();
             CreateChunkMeshData();
             CreateMesh();
         }
 
         public bool IsActive
         {
-            get => chunkObject.activeSelf;
-            set => chunkObject.SetActive(value);
+            get => _chunkObject.activeSelf;
+            set => _chunkObject.SetActive(value);
         }
 
-        public Vector3 Position => chunkObject.transform.position;
+        public Vector3 Position => _chunkObject.transform.position;
 
         /**
      * Populates the block id array of this chunk with the information from world generation.
      */
         private void PopulateVoxelMap()
         {
-            for (var y = 0; y < VoxelData.ChunkHeight; y++)
+            for (var x = 0; x < _blockIdArray.Length; x++)
             {
-                for (var x = 0; x < VoxelData.ChunkWidth; x++)
+                _blockIdArray[x] = new ushort[VoxelData.ChunkHeight][];
+                for (var y = 0; y < _blockIdArray[x].Length; y++)
                 {
-                    for (var z = 0; z < VoxelData.ChunkWidth; z++)
+                    _blockIdArray[x][y] = new ushort[VoxelData.ChunkWidth];
+                    for (var z = 0; z < _blockIdArray[x][y].Length; z++)
                     {
-                        blockIdArray[x, y, z] =
-                            world.GetVoxel(new Vector3(x, y, z) + Position);
-                    }
-                }
-            }
-        }
-
-        /**
-     * Populates the block id array of this chunk with the information from world generation.
-     */
-        private void PopulateVoxelMap2()
-        {
-            for (var x = 0; x < blockIdArray2.Length; x++)
-            {
-                blockIdArray2[x] = new ushort[VoxelData.ChunkHeight][];
-                for (var y = 0; y < blockIdArray2[x].Length; y++)
-                {
-                    blockIdArray2[x][y] = new ushort[VoxelData.ChunkWidth];
-                    for (var z = 0; z < blockIdArray2[x][y].Length; z++)
-                    {
-                        blockIdArray2[x][y][z] =
-                            world.GetVoxel(new Vector3(x, y, z) + Position);
+                        _blockIdArray[x][y][z] =
+                            World.GetVoxel(new Vector3(x, y, z) + Position);
                     }
                 }
             }
@@ -91,15 +69,16 @@ namespace CompanyNine.Voxel.Chunk
 
         private void CreateChunkMeshData()
         {
-            for (var y = 0;
-                y < VoxelData.ChunkHeight;
-                y++)
+            for (var y = 0; y < VoxelData.ChunkHeight; y++)
             {
                 for (var x = 0; x < VoxelData.ChunkWidth; x++)
                 {
                     for (var z = 0; z < VoxelData.ChunkWidth; z++)
                     {
-                        AddVoxelDataToChunk(new Vector3Int(x, y, z));
+                        if (_world.blockTypes[_blockIdArray[x][y][z]].IsSolid)
+                        {
+                            AddVoxelDataToChunk(new Vector3Int(x, y, z));
+                        }
                     }
                 }
             }
@@ -107,7 +86,8 @@ namespace CompanyNine.Voxel.Chunk
 
         private void AddVoxelDataToChunk(Vector3Int blockPosition)
         {
-            var faces = (VoxelData.Face[]) Enum.GetValues(typeof(VoxelData.Face));
+            var faces =
+                (VoxelData.Face[]) Enum.GetValues(typeof(VoxelData.Face));
             foreach (var face in faces)
             {
                 // Checks if this face should be drawn by looking at the voxel that would be next to that face. If the value
@@ -121,36 +101,37 @@ namespace CompanyNine.Voxel.Chunk
                 // var blockId = blockIdArray[blockPosition.x, blockPosition.y,
                 //     blockPosition.z];
 
-                var blockId = blockIdArray2[blockPosition.x][blockPosition
+                var blockId = _blockIdArray[blockPosition.x][blockPosition
                     .y][blockPosition.z];
 
-                var blockTexture = world.blockTypes[blockId].GetBlockTexture(face);
+                var blockTexture =
+                    _world.blockTypes[blockId].GetBlockTexture(face);
 
                 if (blockTexture != null)
                 {
                     AddTexture(blockTexture.Id);
                 }
 
-                vertices.Add(blockPosition + VoxelData.VoxelVertices[VoxelData
+                _vertices.Add(blockPosition + VoxelData.VoxelVertices[VoxelData
                     .VoxelTriangles[(int) face, 0]]);
-                vertices.Add(blockPosition + VoxelData.VoxelVertices[VoxelData
+                _vertices.Add(blockPosition + VoxelData.VoxelVertices[VoxelData
                     .VoxelTriangles[(int) face, 1]]);
-                vertices.Add(blockPosition + VoxelData.VoxelVertices[VoxelData
+                _vertices.Add(blockPosition + VoxelData.VoxelVertices[VoxelData
                     .VoxelTriangles[(int) face, 2]]);
-                vertices.Add(blockPosition + VoxelData.VoxelVertices[VoxelData
+                _vertices.Add(blockPosition + VoxelData.VoxelVertices[VoxelData
                     .VoxelTriangles[(int) face, 3]]);
 
 
                 // These values correspond to the indices of the vertex array we have created above.
-                // We are reusing the second and third vertex on each face as the first two vertices of the second triangle
+                // We are reusing the second and third vertex on each face as the first two vertices of the second triangle 
                 // to save space.
-                triangles.Add(vertexIndex);
-                triangles.Add(vertexIndex + 1);
-                triangles.Add(vertexIndex + 2);
-                triangles.Add(vertexIndex + 2);
-                triangles.Add(vertexIndex + 1);
-                triangles.Add(vertexIndex + 3);
-                vertexIndex += 4;
+                _triangles.Add(_vertexIndex);
+                _triangles.Add(_vertexIndex + 1);
+                _triangles.Add(_vertexIndex + 2);
+                _triangles.Add(_vertexIndex + 2);
+                _triangles.Add(_vertexIndex + 1);
+                _triangles.Add(_vertexIndex + 3);
+                _vertexIndex += 4;
             }
         }
 
@@ -171,13 +152,14 @@ namespace CompanyNine.Voxel.Chunk
             if (!IsVoxelInChunk(x, y, z))
             {
                 // if the voxel is not in this chunk, then retrieve its id from the world and check if its solid or not.
-                return world
-                    .blockTypes[world.GetVoxel(Position + neighborBlockPosition)]
+                return _world
+                    .blockTypes[
+                        World.GetVoxel(Position + neighborBlockPosition)]
                     .IsSolid;
-            }
+            } 
 
             // if the voxel is in the chunk then pull its coordinate and check if its solid or not.
-            return world.blockTypes[blockIdArray2[x][y][z]].IsSolid;
+            return _world.blockTypes[_blockIdArray[x][y][z]].IsSolid;
         }
 
         /**
@@ -185,9 +167,9 @@ namespace CompanyNine.Voxel.Chunk
      */
         private static bool IsVoxelInChunk(int x, int y, int z)
         {
-            if (x < 0 || x > VoxelData.ChunkWidth - 1 || y < 0 ||
-                y > VoxelData.ChunkHeight - 1 || z < 0 ||
-                z > VoxelData.ChunkWidth - 1)
+            if (x < 0 || x >= VoxelData.ChunkWidth || y < 0 ||
+                y >= VoxelData.ChunkHeight || z < 0 ||
+                z >= VoxelData.ChunkWidth )
             {
                 return false;
             }
@@ -199,13 +181,13 @@ namespace CompanyNine.Voxel.Chunk
         {
             var mesh = new Mesh
             {
-                vertices = vertices.ToArray(),
-                triangles = triangles.ToArray(),
-                uv = uvs.ToArray()
+                vertices = _vertices.ToArray(),
+                triangles = _triangles.ToArray(),
+                uv = _uvs.ToArray()
             };
 
             mesh.RecalculateNormals();
-            meshFilter.mesh = mesh;
+            _meshFilter.mesh = mesh;
         }
 
         private void AddTexture(int textureId)
@@ -216,10 +198,10 @@ namespace CompanyNine.Voxel.Chunk
             x *= VoxelData.NormalizedBlockTextureSize;
             y *= VoxelData.NormalizedBlockTextureSize;
             y = 1f - y - VoxelData.NormalizedBlockTextureSize;
-            uvs.Add(new Vector2(x, y));
-            uvs.Add(new Vector2(x, y + VoxelData.NormalizedBlockTextureSize));
-            uvs.Add(new Vector2(x + VoxelData.NormalizedBlockTextureSize, y));
-            uvs.Add(new Vector2(x + VoxelData.NormalizedBlockTextureSize,
+            _uvs.Add(new Vector2(x, y));
+            _uvs.Add(new Vector2(x, y + VoxelData.NormalizedBlockTextureSize));
+            _uvs.Add(new Vector2(x + VoxelData.NormalizedBlockTextureSize, y));
+            _uvs.Add(new Vector2(x + VoxelData.NormalizedBlockTextureSize,
                 y + VoxelData.NormalizedBlockTextureSize));
         }
 
