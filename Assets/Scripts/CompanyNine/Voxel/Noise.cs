7dc997a3c1ac112fb7efdb45cd25e068f3dfb811
Jonathon Bowyer <jwbowyer@gmail.com>
2020-09-20 00:26:08 -0500

    Added Noise generation utility

---
+++ Assets/Scripts/CompanyNine/Voxel/Noise.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace CompanyNine.Voxel
+{
+    public class Noise
+    {
+        public static float Get2DPerlinNoise(Vector2 position, float offset,
+            float scale)
+        {
+            var posX = (position.x + .1f) / VoxelData.ChunkWidth
+                * scale + offset;
+            var posY = (position.y + .1f) / VoxelData.ChunkWidth
+                * scale + offset;
+            return Mathf.PerlinNoise(posX, posY);
+        }
+    }
+}
